0e26bd6b372590826d5255c800b0e650b0d83449
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-09-14 21:58:49 -0300

    add exemplo de um component simples

---
+++ 3-components/estudos-components/src/app/components/hello.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: "hello",
+  templateUrl:"./hello.component.html",
+  styleUrls: ["./hello.component.css"]
+})
+
+
+export class HelloComponent {}
