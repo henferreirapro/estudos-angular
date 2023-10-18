5238fcec11ff896f1d2c49f9217ef1725783beb6
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-18 11:22:16 -0300

    importando o home e portifolio modules no app.module

--- 5-roteamento-angular/rotas/src/app/app.module.ts
+++ 5-roteamento-angular/rotas/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeModule } from './pages/index/home.module';
+import { PortifolioModule } from './pages/portifolio/portifolio.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    PortifolioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
