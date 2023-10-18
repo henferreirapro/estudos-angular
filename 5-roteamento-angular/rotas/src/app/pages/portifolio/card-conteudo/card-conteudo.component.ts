ca7d8ab0f483ab2ac9e59b83274cad7d210eb184
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-18 11:00:32 -0300

    add na pages o portifolio.module e o card-conteudo.component

---
+++ 5-roteamento-angular/rotas/src/app/pages/portifolio/card-conteudo/card-conteudo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-conteudo',
+  templateUrl: './card-conteudo.component.html',
+  styleUrls: ['./card-conteudo.component.css']
+})
+export class CardConteudoComponent {
+
+}
---
+++ 5-roteamento-angular/rotas/src/app/pages/portifolio/portifolio.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardConteudoComponent } from './card-conteudo/card-conteudo.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardConteudoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PortifolioModule { }
