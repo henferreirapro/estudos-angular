5dfda2eb83d24593345ba41cb02f16d5652568b6
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-19 17:11:01 -0300

    add as paths que iremos usar para o roteamento
    Nesse caso a path padrão chamará o componente home e a path portifólio chamará o cardConteudo.

--- 5-roteamento-angular/rotas/src/app/app-routing.module.ts
+++ 5-roteamento-angular/rotas/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/index/home/home.component';
+import { CardConteudoComponent } from './pages/portifolio/card-conteudo/card-conteudo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent, pathMatch: "full" },
+  {path: "portifolio", component: CardConteudoComponent, pathMatch: "prefix"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
