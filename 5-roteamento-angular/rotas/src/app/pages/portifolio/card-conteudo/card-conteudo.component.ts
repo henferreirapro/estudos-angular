dd0a3217aedef6130f91778ce328f8a5f9dfbe42
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-19 20:15:28 -0300

    redirecionamento diretamente no componente
    Aqui nós podemos usar o `Router` direto no arquivo TS do nosso componente quando precisamos redirecionar a nossa pagina para voltar a pagina principal ou outra pagina.
    
    Por exemplo quando o usuário preenche um formulário e depois nós redirecionamos ele para uma pagina de agradecimento ou para a pagina inicial do projeto.
    
    Aqui na linha 12 nós criamos uma variavel chamada navegador que recebe o nosso `Router`.
    
    Na linha 30 nós chamamos a nossa variavel navegador e executamos o navigate dizendo qual é a rota que ele irá executar.
    
    Nós usamos o setInterval para que isso aconteça depois de alguns segundos, no caso 5s e 3s.

--- 5-roteamento-angular/rotas/src/app/pages/portifolio/card-conteudo/card-conteudo.component.ts
+++ 5-roteamento-angular/rotas/src/app/pages/portifolio/card-conteudo/card-conteudo.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-conteudo',
   templateUrl: './card-conteudo.component.html',
   styleUrls: ['./card-conteudo.component.css']
 })
-export class CardConteudoComponent {
-  constructor(private dadosDaRota: ActivatedRoute) {
+export class CardConteudoComponent implements OnInit, OnChanges{
+  constructor(
+    private dadosDaRota: ActivatedRoute,
+    private navegador: Router
+    ) {
 
     // Recuperando dados usando o params
     this.dadosDaRota.params.subscribe(
@@ -19,5 +22,20 @@ export class CardConteudoComponent {
       res => console.log(res)
     )
   }
+
+  ngOnInit(): void {
+    
+    // ira forçar o retorno para a pagina inicial depois de 5s
+    setInterval( () => {
+      this.navegador.navigate(["/"])
+    }, 5000)
+  }
+
+  ngOnChanges(): void { 
+    setInterval( () => {
+      this.navegador.navigate(["/"])
+    }, 3000)
+  }
+
 
 }
