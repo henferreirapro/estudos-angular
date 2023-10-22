9dd2251de9b265619872462e0bfa727a82976ef1
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-22 17:47:59 -0300

    criando componente card-pokemon

--- 8-services-angular/services/src/app/app.module.ts
+++ 8-services-angular/services/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardPokemonComponent } from './components/card-pokemon/card-pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardPokemonComponent
   ],
   imports: [
     BrowserModule,
